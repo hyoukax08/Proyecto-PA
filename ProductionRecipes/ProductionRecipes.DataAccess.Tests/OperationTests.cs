9317616f9645132569b9fd49a1127ec7a6fa943b
OscarAuto <leyvaoscar734@gmail.com>
2024-07-02 23:05:43 -0400

    Creacion de Unit Tests incompleta

---
+++ ProductionRecipes/ProductionRecipes.DataAccess.Tests/OperationTests.cs
@@ -0,0 +1,48 @@
+﻿using ProductionRecipes.Domain.Entities;
+using ProductionRecipes.Domain.Entities.Types;
+using ProductionRecipes.Domain.Entities.AcccionElements;
+using ProductionRecipes.DataAccess.Tests.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System;
+
+namespace ProductionRecipes.DataAccess.Tests
+{
+    [TestClass]
+    public class OperationTests
+    {
+
+        private IOperationRepository _operationRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public OperationTests()
+        {
+            ApplicationContext context =
+            new ApplicationContext(ConnectionStringProvider.GetConnectionString());
+            _operationRepository = new OperationRepository(context);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        [TestMethod]
+        [DataRow(?)]
+        [DataRow(?)]
+        public void Can_Create_Operation(List<Fase> execFases, string name, string description)
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Operation operation = new Operation(execFases, name, description, id);
+
+            // Execute
+            _operationRepository.AddOperation(operation);
+            _unitOfWork.SaveChanges();
+
+            // Assert
+            Product? loadedOperation = _operationRepository.GetOperationById<Operation>(id);
+            Assert.IsNotNull(loadedOperation);
+        }
+
+
+
+
+
+    }
---
+++ ProductionRecipes/ProductionRecipes.DataAccess.Tests/RecipeTests.cs
@@ -0,0 +1,48 @@
+﻿using ProductionRecipes.Domain.Entities;
+using ProductionRecipes.Domain.Entities.Types;
+using ProductionRecipes.Domain.Entities.AcccionElements;
+using ProductionRecipes.DataAccess.Tests.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System;
+
+namespace ProductionRecipes.DataAccess.Tests
+{
+    [TestClass]
+    public class RecipeTests
+    {
+
+        private IRecipeRepository _recipeRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public RecipeTests()
+        {
+            ApplicationContext context =
+            new ApplicationContext(ConnectionStringProvider.GetConnectionString());
+            _recipeRepository = new RecipeRepository(context);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        [TestMethod]
+        [DataRow(?)]
+        [DataRow(?)]
+        public void Can_Create_Product(Product producttomake, List<Operation> execOperation)
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            Recipe recipe = new Recipe(producttomake, execOperation, id);
+
+            // Execute
+            _recipeRepository.AddRecipe(recipe);
+            _unitOfWork.SaveChanges();
+
+            // Assert
+            Recipe? loadedRecipe = _recipeRepository.GetProductById<Recipe>(id);
+            Assert.IsNotNull(loadedRecipe);
+        }
+
+
+
+
+
+    }
