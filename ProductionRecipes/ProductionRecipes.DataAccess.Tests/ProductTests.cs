c4d9dedc3636e719b2698d30f7defb0589297465
OscarAuto <leyvaoscar734@gmail.com>
2024-07-03 23:51:32 -0400

    Terminando Unit Test

--- ProductionRecipes/ProductionRecipes.DataAccess.Tests/ProductTests.cs
+++ ProductionRecipes/ProductionRecipes.DataAccess.Tests/ProductTests.cs
@@ -1,48 +1,124 @@
-﻿using ProductionRecipes.Domain.Entities;
-using ProductionRecipes.Domain.Entities.Types;
-using ProductionRecipes.Domain.Entities.AcccionElements;
+﻿using ProductionRecipes.Domain.Types;
+using ProductionRecipes.Contracts;
+using ProductionRecipes.DataAccess.Contexts;
+using ProductionRecipes.DataAccess.Repositories.Products;
+using ProductionRecipes.Domain.Entities.Products;
+using ProductionRecipes.Domain.Entities.AccionElements.Fases;
+using ProductionRecipes.Domain.Entities.AccionElements.Operations;
+using ProductionRecipes.Domain.ValueObjects.ControlActions;
 using ProductionRecipes.DataAccess.Tests.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace ProductionRecipes.DataAccess.Tests
-{
-    [TestClass]
-    public class ProductTests
     {
-
-        private IProductRepository _productRepository;
-        private IUnitOfWork _unitOfWork;
-
-        public ProductTests()
+        [TestClass]
+        public class ProductTests
         {
-            ApplicationContext context =
-            new ApplicationContext(ConnectionStringProvider.GetConnectionString());
-            _productRepository = new ProductRepository(context);
-            _unitOfWork = new UnitOfWork(context);
+
+            private IProductRepository _productRepository;
+            private IUnitOfWork _unitOfWork;
+
+
+            public ProductTests()
+            {
+                ApplicationContext context =
+                new ApplicationContext(ConnectionStringProvider.GetConnectionString());
+                _productRepository = new ProductRepository(context);
+                _unitOfWork = new UnitOfWork(context);
+            }
+
+            [DataRow("Producto de prueba 1")]
+            [DataRow("Producto de prueba 2")]
+            [TestMethod]
+            public void Can_Create_Product(string name)
+            {
+                // Arrange
+                Guid id = Guid.NewGuid();
+                Product product1 = new(name, id);
+
+                // Execute
+                _productRepository.AddProduct(product1);
+                _unitOfWork.SaveChanges();
+
+                // Assert
+                Product? loadedProduct = _productRepository.GetProductById<Product>(id);
+                Assert.IsNotNull(loadedProduct);
+            }
+
+            [DataRow(0)]
+            [TestMethod]
+            public void Can_Get_Product_By_Id(int position)
+            {
+                // Arrange
+                var products = _productRepository.GetAllProducts<Product>().ToList();
+                Assert.IsNotNull(products);
+                Assert.IsTrue(position < products.Count);
+                Product productToGet = products[position];
+
+                // Execute
+                Product? loadedProduct = _productRepository.GetProductById<Product>(productToGet.Id);
+
+                // Assert
+                Assert.IsNotNull(loadedProduct);
+            }
+
+            [TestMethod]
+            public void Cannot_Get_Product_By_Invalid_Id()
+            {
+                // Arrange
+
+                // Execute
+                Product? loadedProduct = _productRepository.GetProductById<Product>(Guid.Empty);
+
+                // Assert
+                Assert.IsNull(loadedProduct);
+            }
+          
+            [DataRow(0, "Unidad inicial")]
+            [TestMethod]
+            public void Can_Update_Product(int position, string name)
+            {
+                // Arrange
+                var product = _productRepository.GetAllProducts<Product>().ToList();
+                Assert.IsNotNull(product);
+                Assert.IsTrue(position < product.Count);
+                Product productToUpdate = product[position];
+
+                // Execute
+                productToUpdate.Name = name;
+                _productRepository.UpdateProduct(productToUpdate);
+                _unitOfWork.SaveChanges();
+
+                // Assert
+                Product? loadedProduct = _productRepository.GetProductById<Product>(productToUpdate.Id);
+                Assert.IsNotNull(loadedProduct);
+                Assert.AreEqual(loadedProduct.Name, name);
+            }
+
+            [DataRow(0)]
+            [TestMethod]
+            public void Can_Delete_Product(int position)
+            {
+                // Arrange
+                var products = _productRepository.GetAllProducts<Product>().ToList();
+                Assert.IsNotNull(products);
+                Assert.IsTrue(position < products.Count);
+                Product productToDelete = products[position];
+
+                // Execute
+                _productRepository.DeleteProduct(productToDelete);
+                _unitOfWork.SaveChanges();
+
+                // Assert
+                Product? loadedProduct = _productRepository.GetProductById<Product>(productToDelete.Id);
+                Assert.IsNull(loadedProduct);
+            }
+
         }
-
-        [TestMethod]
-        [DataRow(?)]
-        [DataRow(?)]
-        public void Can_Create_Product(string name)
-        {
-            // Arrange
-            Guid id = Guid.NewGuid();
-            Product product = new Product(name, id);
-
-            // Execute
-            _productRepository.AddProduct(product);
-            _unitOfWork.SaveChanges();
-
-            // Assert
-            Product? loadedProduct = _productRepository.GetProductById<Product>(id);
-            Assert.IsNotNull(loadedProduct);
-        }
-
-        
-
-
 
     }
--- ProductionRecipes/ProductionRecipes.DataAccess.Tests/RecipeTests.cs
+++ ProductionRecipes/ProductionRecipes.DataAccess.Tests/RecipeTests.cs
@@ -1,11 +1,20 @@
-﻿using ProductionRecipes.Domain.Entities;
-using ProductionRecipes.Domain.Entities.Types;
-using ProductionRecipes.Domain.Entities.AcccionElements;
+﻿using ProductionRecipes.Domain.Types;
+using ProductionRecipes.Contracts;
+using ProductionRecipes.DataAccess.Contexts;
+using ProductionRecipes.DataAccess.Repositories.Recipes;
+using ProductionRecipes.Domain.Entities.Recipe;
+using ProductionRecipes.Domain.Entities.Products;
+using ProductionRecipes.Domain.Entities.AccionElements.Fases;
+using ProductionRecipes.Domain.Entities.AccionElements.Operations;
+using ProductionRecipes.Domain.ValueObjects.ControlActions;
 using ProductionRecipes.DataAccess.Tests.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace ProductionRecipes.DataAccess.Tests
 {
     [TestClass]
@@ -14,6 +23,7 @@ namespace ProductionRecipes.DataAccess.Tests
 
         private IRecipeRepository _recipeRepository;
         private IUnitOfWork _unitOfWork;
+
 
         public RecipeTests()
         {
@@ -24,40 +34,89 @@ namespace ProductionRecipes.DataAccess.Tests
         }
 
         [TestMethod]
-        [DataRow(?)]
-        [DataRow(?)]
-        public void Can_Create_Product(Product producttomake)
+        public void Can_Create_Recipe(Product producttomake, List<Operation> execOperation)
         {
             // Arrange
             Guid id = Guid.NewGuid();
-            Recipe recipe = new Recipe(producttomake, execOperation, id);
+            List<Operation> listoperation1 = new List<Operation>();
+            Product product1 = new Product("prueba", id);
+            Recipe recipe1 = new(product1, listoperation1, id);
 
             // Execute
-            _recipeRepository.AddRecipe(recipe);
+            _recipeRepository.AddRecipe(recipe1);
             _unitOfWork.SaveChanges();
 
             // Assert
-            Recipe? loadedRecipe = _recipeRepository.GetProductById<Recipe>(id);
+            Recipe? loadedRecipe = _recipeRepository.GetRecipeById<Recipe>(id);
             Assert.IsNotNull(loadedRecipe);
         }
-        [DataRow()]
-        public void Can_Get_Id_Of_Product_To_Make(int position)
+
+        [DataRow(1)]
+        [TestMethod]
+        public void Can_Get_Recipe_By_Id(int position)
         {
             // Arrange
-            var recipes = _accionElementRepository.GetAllRecipes().ToList();
+            var recipes = _recipeRepository.GetAllRecipes<Recipe>().ToList();
             Assert.IsNotNull(recipes);
             Assert.IsTrue(position < recipes.Count);
-            Recipe recipeToGetProductIdFrom = recipes[position];
+            Recipe recipeToGet = recipes[position];
 
             // Execute
-            Recipe? loadedRecipe = _accionElementRepository.GetRecipeById(recipeToGetProductIdFrom.Id);
-            Guid? productid = loadedRecipe.ProductId
+            Recipe? loadedRecipe = _recipeRepository.GetRecipeById<Recipe>(recipeToGet.Id);
 
             // Assert
-            Assert.IsNotNull(productid);
+            Assert.IsNotNull(loadedRecipe);
         }
+        
+        public void Cannot_Get_Recipe_By_Invalid_Id()
+        {
+            // Arrange
+
+            // Execute
+            Recipe? loadedRecipe = _recipeRepository.GetRecipeById<Recipe>(Guid.Empty);
+
+            // Assert
+            Assert.IsNull(loadedRecipe);
+        }
+
+        public void Can_Update_Recipe(Product producttomake, List<Operation> execOperation)
+        {
+            // Arrange
+            var recipes = _recipeRepository.GetAllRecipes<Recipe>().ToList();
+            Assert.IsNotNull(recipes);
+            Assert.IsTrue(position < recipes.Count);
+            Recipe recipeToUpdate = recipes[position];
+
+            // Execute
+            recipeToUpdate.UnityName = unityname;
+            _recipeRepository.UpdateRecipe(recipeToUpdate);
+            _unitOfWork.SaveChanges();
+
+            // Assert
+            Recipe? loadedRecipe = _recipeRepository.GetRecipeById<Recipe>(recipeToUpdate.Id);
+            Assert.IsNotNull(loadedRecipe);
+            Assert.AreEqual(loadedRecipe.UnityName, unityname);
+        }
+
+        [DataRow(0)]
+        [TestMethod]
+        public void Can_Delete_Recipe(int position)
+        {
+            // Arrange
+            var recipes = _recipeRepository.GetAllRecipes<Recipe>().ToList();
+            Assert.IsNotNull(recipes);
+            Assert.IsTrue(position < recipes.Count);
+            Recipe recipeToDelete = recipes[position];
+
+            // Execute
+            _recipeRepository.DeleteRecipe(recipeToDelete);
+            _unitOfWork.SaveChanges();
+
+            // Assert
+            Recipe? loadedRecipe = _recipeRepository.GetRecipeById<Recipe>(recipeToDelete.Id);
+            Assert.IsNull(loadedRecipe);
+        }
+
     }
 
-
-
-    }
+}
